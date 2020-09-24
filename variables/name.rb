7d9553dfb1c2c8358761381aec185e80821ba400
Thanda Myo Win <thandamwin@gmail.com>
2020-09-24 08:25:09 +0000

    Variables exercises

---
+++ variables/name.rb
@@ -0,0 +1,23 @@
+# Exercise 1
+
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "Hello there " + name
+
+# Exercise 3
+
+10.times {puts name}
+
+# Exercise 4
+
+puts "Again, what is your first name?"
+
+first_name = gets.chomp
+
+puts "Again, what is your last name?"
+
+last_name = gets.chomp
+
+puts first_name + " " + last_name
